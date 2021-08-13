4c979cfbea0c983737087be0bf21da57b1df5312
mominahmed23 <mominahmad324@gmail.com>
2021-08-13 12:00:44 +0500

    in progress

--- src/components/Profile/PerDetail.js
+++ src/components/Profile/PerDetail.js
@@ -8,58 +8,76 @@ const { Text, Link } = Typography;
 function PerDetail() {
   return (
     <>
-      <div className="head site-card-wrapper mt-5 ">
-        <h3>Personal Details</h3>
-        <EditOutlined className="editicon" />
-        <div className="mt-5">
-          <Row gutter={16}>
-            <Col span={8}>
-              <div className>
-                <Text type="secondary">Full Name</Text>
-                <br></br>
-                <Text strong>Ali Raza</Text>
-              </div>
-              <div className="mt-8">
-                <Text type="secondary">Full Name</Text>
-                <br></br>
-                <Text strong>Ali Raza</Text>
-              </div>
-              <div className="mt-8">
-                <Text type="secondary">Full Name</Text>
-                <br></br>
-                <Text strong>Ali Raza</Text>
-              </div>
-            </Col>
-            <Col span={8}>
-              <div className>
-                <Text type="secondary">Gender Name</Text>
-                <br></br>
-                <Text strong>Male</Text>
-              </div>
-              <div className="mt-8">
-                <Text type="secondary">Contact Number</Text>
-                <br></br>
-                <Text strong>Ali Raza</Text>
-              </div>
-              <div className="mt-8">
-                <Text type="secondary">Aadhaar Number</Text>
-                <br></br>
-                <Text strong>Ali Raza</Text>
-              </div>
-            </Col>
-            <Col span={8}>
-              <div className>
-                <Text type="secondary">Brithday</Text>
-                <br></br>
-                <Text strong>27th September 1995</Text>
-              </div>
-              <div className="mt-8">
-                <Text type="secondary">Alternate Contact Number</Text>
-                <br></br>
-              </div>
-            </Col>
-          </Row>
+      <div className="main-container">
+        <div>
+          <Card className="card-container">
+            <h3>Personal Details</h3>
+            <EditOutlined className="editicon" />
+            <div className="mt-5">
+              <Row gutter={16}>
+                <Col span={8}>
+                  <div className>
+                    <Text type="secondary">Full Name</Text>
+                    <br></br>
+                    <Text strong>Ali Raza</Text>
+                  </div>
+                  <div className="mt-8">
+                    <Text type="secondary">Full Name</Text>
+                    <br></br>
+                    <Text strong>Ali Raza</Text>
+                  </div>
+                  <div className="mt-8">
+                    <Text type="secondary">Full Name</Text>
+                    <br></br>
+                    <Text strong>Ali Raza</Text>
+                  </div>
+                </Col>
+                <Col span={8}>
+                  <div className>
+                    <Text type="secondary">Gender Name</Text>
+                    <br></br>
+                    <Text strong>Male</Text>
+                  </div>
+                  <div className="mt-8">
+                    <Text type="secondary">Contact Number</Text>
+                    <br></br>
+                    <Text strong>Ali Raza</Text>
+                  </div>
+                  <div className="mt-8">
+                    <Text type="secondary">Aadhaar Number</Text>
+                    <br></br>
+                    <Text strong>Ali Raza</Text>
+                  </div>
+                </Col>
+                <Col span={8}>
+                  <div className>
+                    <Text type="secondary">Brithday</Text>
+                    <br></br>
+                    <Text strong>27th September 1995</Text>
+                  </div>
+                  <div className="mt-8">
+                    <Text type="secondary">Alternate Contact Number</Text>
+                    <br></br>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Card>
         </div>
+        <div>
+          <Card style={{ marginTop: "25px", marginLeft: "25px", width: "90%" }}>
+            hello
+          </Card>
+        </div>
+        <div>
+          {" "}
+          <Card style={{ marginTop: "25px", marginLeft: "25px", width: "90%" }}>
+            hello
+          </Card>
+        </div>
+      </div>
+      {/* <div className="head site-card-wrapper mt-5 ">
+       
       </div>
 
       <div
@@ -220,7 +238,7 @@ function PerDetail() {
             </Col>
           </Row>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
