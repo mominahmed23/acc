f3a4af0949343d2d784be66ab153d831aa0bbc06
mominahmed23 <mominahmad324@gmail.com>
2021-08-17 13:46:06 +0000

    Customization-Primary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "./App.less";
+// import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "antd-css-utilities/utility.min.css";
+import { Button , Typography } from "antd";
 import Routes from "./routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Typography.Text type="success">Customization</Typography.Text>
+      <Button type='primary'>jjj</Button><br></br>
+      <Typography.Text type="danger">Not Customization</Typography.Text>
+      <Button danger>jjj</Button>
+    </>
+  );
 }
 
 export default App;
