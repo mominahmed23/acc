f62f1c6919154d496985a63d09f64b0b80ae4530
mominahmed23 <mominahmad324@gmail.com>
2021-08-18 19:40:23 +0000

    work on ....PaymtInvoice

--- src/components/MyPayment/PaymentInvoice.jsx
+++ src/components/MyPayment/PaymentInvoice.jsx
@@ -1,10 +1,9 @@
-import { Col, Row, Table, Typography } from 'antd'
+import { Col, Row, Table, Typography } from "antd";
 
-import React,{ useState } from 'react';
-
+import React, { useState } from "react";
 
 export default function PaymentInvoice() {
-    const [selectionType, setSelectionType] = useState("checkbox");
+  const [selectionType, setSelectionType] = useState("checkbox");
   const columns = [
     {
       title: "Client",
@@ -42,8 +41,7 @@ export default function PaymentInvoice() {
       name: "Ali Raza",
       age: 32,
       address: "New York No. 1 Lake Park",
-    }
-  
+    },
   ];
 
   const rowSelection = {
@@ -59,64 +57,81 @@ export default function PaymentInvoice() {
     }),
   };
 
-    return (
-        <>
-          <Row>
-              <Col xs={20} sm={24} md={24} lg={24}>
-                 <div className="d-flex"> <Typography.Title level={5}>In voice:21</Typography.Title>&nbsp;&nbsp;<Typography.Text >(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text></div>
-              </Col>
-              <Col  xs={24} sm={24} md={19} lg={19} >
-                  <div className="d-flex flex-column">
-              <Typography.Title level={5}>For:</Typography.Title>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              </div>
-              </Col>
-              <Col  xs={24} sm={24} md={5} lg={5} >
-                  <div className="d-flex flex-column">
-              <Typography.Title level={5}>For:</Typography.Title>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              </div>
-              </Col>
-              <Col  xs={24} sm={24} md={22} lg={22} >
-                  <div className="d-flex justify-center ml-5">
-              <Table pagination={false} columns={columns} dataSource={data} />
-              </div>
-              </Col>
-              <Col xs={24} sm={24} md={12} lg={12} >  
-              <div className="d-flex flex-column">
-              <Typography.Title level={5}>For:</Typography.Title>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              </div>
-              </Col>
-              <Col xs={24} sm={24} md={12} lg={12} >
-              <div className="d-flex flex-column">
-              <Typography.Title level={5}>For:</Typography.Title>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
-              </div>
-              </Col>
+  return (
+    <>
+      <Row className="px-5 py-1">
+        <Col xs={24} sm={24} md={24} lg={24}>
+          <div className="d-flex">
+            {" "}
+            <Typography.Title level={5}>In&nbsp;voice:21</Typography.Title>
+            &nbsp;&nbsp;
+            <Typography.Text>(1&nbsp;Feb,&nbsp;2010&nbsp;-&nbsp;10&nbsp;Feb,&nbsp;2010)</Typography.Text>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={18}>
+          <div className="d-flex flex-column">
+            <Typography.Title level={5}>For:</Typography.Title>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={6} className="d-flex justify-end">
+          <div className="d-flex flex-column">
+            <Typography.Title level={5}>For:</Typography.Title>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={22} lg={22}>
+          <div className="d-flex justify-center ml-5">
+            <Table
+              style={{ border: "1px solid #808080" }}
+              pagination={false}
+              columns={columns}
+              dataSource={data}
+            />
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <div className="d-flex flex-column">
+            <Typography.Title level={5}>For:</Typography.Title>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <div className="d-flex flex-column">
+            <Typography.Text>
+              {" "}
+              <hr />
+              (1 Feb, 2010 - 10 Feb, 2010)
+            </Typography.Text>
+            <Typography.Text>
+              (1 Feb, 2010 - 10 Feb, 2010) <hr />
+              <hr />
+            </Typography.Text>
 
-              </Row>  
-        </>
-    )
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+            <Typography.Text>
+              (1 Feb, 2010 - 10 Feb, 2010) <hr />
+            </Typography.Text>
+
+            <Typography.Text>(1 Feb, 2010 - 10 Feb, 2010)</Typography.Text>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 }
- 
